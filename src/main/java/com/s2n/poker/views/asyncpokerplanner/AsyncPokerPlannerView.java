2bb6be0d37c741efa8ece0ae8ed769c8dd356175
start.vaadin.com <no-reply@vaadin.com>
2024-12-18 15:54:59 +0000

    Generated project

---
+++ src/main/java/com/s2n/poker/views/asyncpokerplanner/AsyncPokerPlannerView.java
@@ -0,0 +1,47 @@
+package com.s2n.poker.views.asyncpokerplanner;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+
+@PageTitle("Async Poker Planner")
+@Route("")
+public class AsyncPokerPlannerView extends Composite<VerticalLayout> {
+
+    public AsyncPokerPlannerView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        TextField textField = new TextField();
+        Button buttonPrimary = new Button();
+        HorizontalLayout layoutRow2 = new HorizontalLayout();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.setHeight("min-content");
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        textField.setLabel("Room Name");
+        layoutColumn2.setAlignSelf(FlexComponent.Alignment.CENTER, textField);
+        textField.setWidth("min-content");
+        buttonPrimary.setText("Create or Join Room");
+        layoutColumn2.setAlignSelf(FlexComponent.Alignment.CENTER, buttonPrimary);
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        layoutRow2.addClassName(Gap.MEDIUM);
+        layoutRow2.setWidth("100%");
+        layoutRow2.setHeight("min-content");
+        getContent().add(layoutRow);
+        getContent().add(layoutColumn2);
+        layoutColumn2.add(textField);
+        layoutColumn2.add(buttonPrimary);
+        getContent().add(layoutRow2);
+    }
+}
